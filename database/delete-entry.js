a0b1cf4ba14498fd429f5fbaa94bb640c340674b
SeanPlusPlus <seanmichaelstephenson@gmail.com>
2024-04-22 22:25:45 -0700

    main: empty del id

--- database/delete-entry.js
+++ database/delete-entry.js
@@ -3,7 +3,7 @@ const { sql } = require("@vercel/postgres")
 require("dotenv").config({ path: ".env.development.local" })
 
 async function main() {
-  const id = "06af137f-1e1c-490c-a5f2-e991cf64eb16"
+  const id = ""
   const result = await sql`DELETE FROM Entries WHERE id=${id};`
   console.log(result)
 }
