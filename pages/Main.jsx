927554815d107466acc8a5a7c50c07bf437a68a0
SeanPlusPlus <seanmichaelstephenson@gmail.com>
2024-04-22 22:16:44 -0700

    main: alert

--- pages/Main.jsx
+++ pages/Main.jsx
@@ -1,11 +1,26 @@
+import { useState } from "react"
 import { DndProvider } from "react-dnd"
 import useSSRBackend from "../hooks/useSSRBackend"
 
 import List from "./List"
+import HowTo from "./HowTo"
+
 import isClosed from "../utils/closed"
+
+import Alert from "react-bootstrap/Alert"
+import Button from "react-bootstrap/Button"
+import Modal from "react-bootstrap/Modal"
 
 export default function Main() {
   const Backend = useSSRBackend()
+  const [show, setShow] = useState(false)
+
+  const handleShow = (e) => {
+    e.preventDefault()
+    setShow(true)
+  }
+
+  const handleClose = () => setShow(false)
 
   if (isClosed) {
     return (
@@ -16,12 +31,34 @@ export default function Main() {
     )
   }
 
+  const Info = (
+    <Modal show={show} onHide={handleClose} size="lg">
+      <HowTo />
+      <Modal.Footer>
+        <Button variant="secondary" onClick={handleClose}>
+          Close
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  )
+
   return (
-    <main>
-      <h1>2024 NFL Draft First Round</h1>
-      <DndProvider backend={Backend}>
-        <List />
-      </DndProvider>
-    </main>
+    <>
+      <main>
+        <h1>2024 NFL Draft First Round</h1>
+        <Alert variant={"info"}>
+          <span role="img" aria-label="hand waving">
+            👋
+          </span>{" "}
+          Make sure to Review{" "}
+          <Alert.Link onClick={handleShow}>How to Play</Alert.Link> before
+          getting started with your entry
+        </Alert>
+        <DndProvider backend={Backend}>
+          <List />
+        </DndProvider>
+      </main>
+      {Info}
+    </>
   )
 }
