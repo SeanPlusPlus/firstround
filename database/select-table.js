fba9717fc64f6e7f7e6174a41fcc931d39388ba3
SeanPlusPlus <seanmichaelstephenson@gmail.com>
2024-04-22 21:41:18 -0700

    main: remove unneeded

--- database/select-table.js
+++
@@ -1,9 +0,0 @@
-const { sql } = require("@vercel/postgres")
-require("dotenv").config({ path: ".env.development.local" })
-
-async function main() {
-  const result = await sql`Select * from Contacts;`
-  console.log(result.rows)
-}
-
-main()
