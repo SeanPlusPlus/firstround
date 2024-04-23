499399a1e3fe1a378141fbbf6f5a741d78d58258
SeanPlusPlus <seanmichaelstephenson@gmail.com>
2024-04-22 17:20:19 -0700

    main: reset data

--- database/add-pick.js
+++ database/add-pick.js
@@ -6,16 +6,17 @@ const YEAR = "2024"
 
 const picks = [
   { rank: 1, name: "Caleb Williams", college: "USC", position: "QB" },
-  { rank: 2, name: "Jayden Daniels", college: "LSU", position: "QB" },
-  { rank: 5, name: "Drake Maye", college: "North Carolina", position: "QB" },
-  { rank: 4, name: "Malik Nabers", college: "LSU", position: "WR" },
-  { rank: 7, name: "Brock Bowers", college: "Georgia", position: "TE" },
-  { rank: 6, name: "Rome Odunze", college: "Washington", position: "WR" },
-  { rank: 11, name: "JC Latham", college: "Alabama", position: "OT" },
-  { rank: 13, name: "Quinyon Mitchell", college: "Toledo", position: "CB" },
-  { rank: 23, name: "Cooper DeJean", college: "Iowa", position: "CB" },
-  { rank: 24, name: "Tyler Guyton", college: "Oklahoma", position: "OT" },
-  { rank: 29, name: "Darius Robinson", college: "Missouri", position: "EDGE" },
+  // { rank: 2, name: "Jayden Daniels", college: "LSU", position: "QB" },
+  // { rank: 5, name: "Drake Maye", college: "North Carolina", position: "QB" },
+  // { rank: 4, name: "Malik Nabers", college: "LSU", position: "WR" },
+  // { rank: 7, name: "Brock Bowers", college: "Georgia", position: "TE" },
+  // { rank: 99, name: "DeWayne Carter", college: "Duke", position: "DT" },
+  // { rank: 6, name: "Rome Odunze", college: "Washington", position: "WR" },
+  // { rank: 11, name: "JC Latham", college: "Alabama", position: "OT" },
+  // { rank: 13, name: "Quinyon Mitchell", college: "Toledo", position: "CB" },
+  // { rank: 23, name: "Cooper DeJean", college: "Iowa", position: "CB" },
+  // { rank: 24, name: "Tyler Guyton", college: "Oklahoma", position: "OT" },
+  // { rank: 29, name: "Darius Robinson", college: "Missouri", position: "EDGE" },
 ]
 
 async function main() {
