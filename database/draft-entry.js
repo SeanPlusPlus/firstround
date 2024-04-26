2fd6105fdbf51d1e7dcfdfd96d611f94399bbbdc
SeanPlusPlus <seanmichaelstephenson@gmail.com>
2024-04-25 17:11:37 -0700

    main: draft entry clean

--- database/draft-entry.js
+++ database/draft-entry.js
@@ -6,23 +6,6 @@ const YEAR = "2024"
 
 const picks = [
   { rank: 1, name: "Caleb Williams", college: "USC", position: "QB" },
-  // { rank: 2, name: "Jayden Daniels", college: "LSU", position: "QB" },
-  // {
-  //   rank: 3,
-  //   name: "Marvin Harrison Jr.",
-  //   college: "Ohio State",
-  //   position: "WR",
-  // },
-  // { rank: 4, name: "Malik Nabers", college: "LSU", position: "WR" },
-  // { rank: 5, name: "Drake Maye", college: "North Carolina", position: "QB" },
-  // { rank: 6, name: "Rome Odunze", college: "Washington", position: "WR" },
-  // { rank: 7, name: "Brock Bowers", college: "Georgia", position: "TE" },
-  // { rank: 8, name: "Dallas Turner", college: "Alabama", position: "EDGE" },
-  // { rank: 9, name: "Joe Alt", college: "Notre Dame", position: "OT" },
-  // { rank: 10, name: "Troy Fautanu", college: "Washington", position: "OT" },
-  // { rank: 11, name: "JC Latham", college: "Alabama", position: "OT" },
-  // { rank: 12, name: "Laiatu Latu", college: "UCLA", position: "EDGE" },
-  // { rank: 13, name: "Quinyon Mitchell", college: "Toledo", position: "CB" },
 ]
 
 async function main() {
